5399be2e07f4c730eb27b3a52b39c14026b6af8c
Soham Patil <145631825+Soham271@users.noreply.github.com>
2025-03-29 22:03:26 +0530

    Create Actions.js

---
+++ Frontend/@latest/src/Actions.js
@@ -0,0 +1,11 @@
+const ACTIONS = {
+  JOIN: "join",
+  JOINED: "joined",
+  DISCONNECTED: "disconnected",
+  CODE_CHANGE: "code_change",
+  SYNC_CODE: "sync_code",
+  LEAVE: "leave",
+  LEAVE_ROOM: "leave_room",
+};
+
+export default ACTIONS;
