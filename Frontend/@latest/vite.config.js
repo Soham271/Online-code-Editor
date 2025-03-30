371c5d3c99007e78e55d624b6c08a0b881abb714
Soham Patil <145631825+Soham271@users.noreply.github.com>
2025-03-30 17:11:54 +0530

    Update vite.config.js

--- Frontend/@latest/vite.config.js
+++ Frontend/@latest/vite.config.js
@@ -4,10 +4,26 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ["file", "system"], // Mark these as external
+    },
+  },
   optimizeDeps: {
     include: [
       "core-js-pure/stable/object/define-properties.js",
       "@babel/runtime/helpers/classCallCheck",
     ],
+    exclude: ["jshints"],
+  },
+  resolve: {
+    alias: {
+      events: "events",
+    },
+  },
+  server: {
+    hmr: {
+      overlay: false, 
+    },
   },
 });
